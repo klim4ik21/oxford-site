9be408b3f1d23c2572ea3409049a80972ddfc98c
klim4ik21 <154627309+klim4ik21@users.noreply.github.com>
2024-01-07 12:16:30 +0300

    Create db.py

---
+++ data/db.py
@@ -0,0 +1,31 @@
+import psycopg2
+import datetime
+from psycopg2.extras import DictCursor
+
+
+db_uri = "postgresql://gen_user:q%2Fgy%3FQy%3F0%3Dfi%5C%3F@109.172.88.61:5432/default_db"
+
+class SQLighter:
+    def __init__(self, db_uri):
+        self.connection = psycopg2.connect(db_uri)
+        self.cursor = self.connection.cursor()
+
+    
+    def create_user(self, username, password_hash, email):
+        # Добавление нового пользователя в базу данных
+        self.cursor.execute('INSERT INTO users (username, password_hash, email) VALUES (%s, %s, %s)', (username, password_hash, email))
+        self.connection.commit()
+
+    def get_user(self, username):
+        with self.connection.cursor(cursor_factory=DictCursor) as cursor:
+            cursor.execute('SELECT * FROM users WHERE username = %s', (username,))
+            return cursor.fetchone()
+    
+    def get_posts(self):
+        self.cursor.execute('''
+            SELECT posts.id, posts.text, posts.image_url, posts.created_at, users.username
+            FROM posts
+            JOIN users ON posts.user_id = users.id
+            ORDER BY posts.created_at DESC
+        ''')
+        return self.cursor.fetchall()
