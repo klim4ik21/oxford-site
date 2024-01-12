82c9352dcc08c0c003e00a5a1766512ff6f3183c
klim4ik21 <154627309+klim4ik21@users.noreply.github.com>
2024-01-12 15:43:48 +0300

    added mobile support

--- routers/home.py
+++ routers/home.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, redirect, url_for
 from data.db import SQLighter
 from flask import session
 from config import db_uri
@@ -19,6 +19,22 @@ def home():
 
     # Получаем список постов из ответа
     posts_list = posts_response.json.get('posts', []) if hasattr(posts_response, 'json') else []
+    user_agent = request.headers.get('User-Agent')
+    if "Mobile" in user_agent:
+        return redirect(url_for('mobile_index'))
+    else:
+        return render_template('index.html', posts=posts_list)
+    
 
-    return render_template('index.html', posts=posts_list)
+@home_bp.route('/m')
+def mobile_index():
+    db = SQLighter(db_uri)
+    if 'username' in session:
+        db.update_last_seen(session['username'])
 
+    # Вызываем функцию get_posts() непосредственно, без HTTP-запроса
+    posts_response = apiv1.get_posts()  # Здесь предполагается, что get_posts возвращает ответ Flask
+
+    # Получаем список постов из ответа
+    posts_list = posts_response.json.get('posts', []) if hasattr(posts_response, 'json') else []
+    return render_template('mobile_index.html', posts=posts_list)
