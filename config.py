7e366dcee30ba2b544fcadf81c99e9a8484c8fb9
Klim <154627309+klim4ik21@users.noreply.github.com>
2024-02-29 14:19:03 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-db_uri = "postgresql://gen_user:q%2Fgy%3FQy%3F0%3Dfi%5C%3F@109.172.88.61:5432/default_db"
+db_uri = "POSTGRESQL DATABASE CONNECTION"
 profile = 1
 
 av_methods = 'get_user', 'get_post'
